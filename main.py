3fb36363a3337d9f5393154772e8e78f5efc02ea
MortikCZ <stareclukas25@gmail.com>
2024-02-20 18:35:51 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,11 +9,10 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command(name='sendmessage')
+@has_permissions(administrator=True)
 async def send_message(ctx, channel: discord.TextChannel, role: discord.Role = None, *, message: str):
-    # Získání odkazu na zprávu
     message_link = await channel.send(f"{role.mention} {message}" if role else message)
 
     await ctx.send(f"Zpráva byla odeslána na kanál {channel.mention} s rolí {role.mention if role else 'bez role'}. Odkaz na zprávu: {message_link.jump_url}")
 
-# Spustí bota s tokenem vašeho bota
-bot.run('MTIwODA0ODAzNTE2NzIxMTU4MQ.G__lMB.hSACpF7vlmBWo28LGSykA0ACnUvgAAkRdzx5bI')
+bot.run('TOKEN')
